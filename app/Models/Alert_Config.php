d3b26ee9dd1e1ef6b0d5cccca8fb8847feb4126d
haruka01152 <haruka.0115maple@yahoo.ne.jp>
2021-09-01 18:00:13 +0900

    9/1 2

---
+++ app/Models/Alert_Config.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Alert_Config extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2021_09_01_172817_create_constructions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConstructionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('constructions', function (Blueprint $table) {
+            $table->id();
+            $table->date('contract_date');
+            $table->date('construction_date');
+            $table->string('customer_name');
+            $table->string('construction_name');
+            $table->integer('alert_config');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('constructions');
+    }
+}
